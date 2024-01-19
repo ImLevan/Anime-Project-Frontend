613b3e3a26f7f027c4961c6af985e5fc05cf5190
ImLevan <chianesevalentin@gmail.com>
2024-01-19 04:11:25 -0300

    fix env variable

--- src/api/API-methods.js
+++ src/api/API-methods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const client = axios.create({
-  baseURL: `${import.meta.env.VITE_URL_BACKEND}`
+  baseURL: `${import.meta.env.VITE_VERCEL_URL_BACKEND}`
   //baseURL: "http://localhost:8080/api/v1",
 });
 
